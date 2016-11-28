dc23ccd0000554c79662599d68c34481284bc954
Octavio Benitez <obenm8@gmail.com>
2016-11-28 05:35:35 -0700

    Connection with MySQL
    // Conexión con MySQL

--- Robofood/app/src/main/java/com/robofood/octavioben/robofood/Menu.java
+++ Robofood/app/src/main/java/com/robofood/octavioben/robofood/Menu.java
@@ -2,6 +2,7 @@ package com.robofood.octavioben.robofood;
 
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,14 +11,57 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
+import org.json.JSONArray;
+
+import java.io.IOException;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Menu extends AppCompatActivity {
 
     ImageButton menu_button_cart, menu_cart_button_close;
     Button menu_cart_button_checkout;
     FrameLayout menu_cart;
 
+    String IP = "http://obenm.esy.es";
+    String GET = "http://obenm.esy.es/get_products.php";
+
+    TextView textViewTest;
+
+    ObtenerWebService hiloconexion;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
@@ -25,6 +69,8 @@ public class Menu extends AppCompatActivity {
         menu_button_cart = (ImageButton) findViewById(R.id.menu_button_cart);
         menu_cart_button_close = (ImageButton) findViewById(R.id.menu_cart_button_close);
         menu_cart_button_checkout = (Button) findViewById(R.id.menu_cart_button_checkout);
+
+        textViewTest = (TextView) findViewById(R.id.textViewTest);
 
         menu_button_cart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +93,138 @@ public class Menu extends AppCompatActivity {
                 startActivity(Menu);
             }
         });
+
+        hiloconexion = new ObtenerWebService();
+        hiloconexion.execute(GET);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Menu Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    public class ObtenerWebService extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String cadena = GET;
+
+            String devuelve = "";
+
+            URL url = null; // Url de donde queremos obtener información
+            try {
+                url = new URL(cadena);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Abrir la conexión
+                connection.setRequestProperty("User-Agent", "Mozilla/5.0" +
+                        " (Linux; Android 1.5; es-ES) Ejemplo HTTP");
+                //connection.setHeader("content-type", "application/json");
+
+                int respuesta = connection.getResponseCode();
+                StringBuilder result = new StringBuilder();
+
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+
+
+                    InputStream in = new BufferedInputStream(connection.getInputStream());  // preparo la cadena de entrada
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));  // la introduzco en un BufferedReader
+
+                    // El siguiente proceso lo hago porque el JSONOBject necesita un String y tengo
+                    // que tranformar el BufferedReader a String. Esto lo hago a traves de un
+                    // StringBuilder.
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);        // Paso toda la entrada al StringBuilder
+                    }
+
+                    //Creamos un objeto JSONObject para poder acceder a los atributos (campos) del objeto.
+                    JSONObject respuestaJSON = new JSONObject(result.toString());   //Creo un JSONObject a partir del StringBuilder pasado a cadena
+                    //Accedemos al vector de resultados
+
+                        JSONArray datosJSON = respuestaJSON.getJSONArray("datos");
+                        for(int i = 0; i < datosJSON.length(); i++){
+                            devuelve = devuelve + datosJSON.getJSONObject(i).getString("idproduct") + " " +
+                                    datosJSON.getJSONObject(i).getString("name") + " " +
+                                    datosJSON.getJSONObject(i).getString("price") + " " +
+                                    datosJSON.getJSONObject(i).getString("details") + " " +
+                                    datosJSON.getJSONObject(i).getString("image") + "\n";
+
+                    }
+
+                    Log.d("return:", devuelve);
+
+
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //Log.d("myTag", devuelve);
+            return devuelve;
+        }
+
+        @Override
+        protected void onCancelled(String aVoid) {
+            super.onCancelled(aVoid);
+        }
+
+        @Override
+        protected void onPostExecute(String aVoid) {
+            //textViewTest.setText(aVoid);
+            //super.onPostExecute(aVoid);
+            //Log.d("myTag", aVoid);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            //resultado.setText("");
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
     }
 }
